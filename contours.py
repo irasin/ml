c4b4cee5c19f1b4e727e34627b22be3b414f81de
edad <edad811@gmail.com>
2017-08-30 16:30:40 +0900

    格式修改

--- contours.py
+++ contours.py
@@ -1,14 +1,14 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-
-def f(x, y):  # the height function
-    return (1 - x/2 + x**5 + y**3) * np.exp(-x**2 - y**2)
-
 n = 256
 x = np.linspace(-3, 3, n)
 y = np.linspace(-3, 3, n)
 X, Y = np.meshgrid(x, y)
+
+
+def f(x, y):  # the height function
+    return (1 - x/2 + x**5 + y**3) * np.exp(-x**2 - y**2)
 
 # use plt.contourf to fill contours
 # X, Y and value for (X, Y) point
