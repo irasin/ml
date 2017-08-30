cdbf9da4f98785c3a4cbeb78c41061d3ab2bb659
edad <edad811@gmail.com>
2017-08-30 16:36:07 +0900

    secondary axis

---
+++ secondary_axis.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.arange(0, 10, 0.1)
+y1 = 0.05*x**2
+y2 = -1*y1
+
+fig, ax1 = plt.subplots()
+ax2 = ax1.twinx()
+ax1.plot(x, y1, 'g-')
+ax2.plot(x, y2, 'b--')
+ax1.set_xlabel('X data')
+ax1.set_ylabel('Y1', color='b')
+ax2.set_ylabel('Y2', color='r')
+
+plt.show()
